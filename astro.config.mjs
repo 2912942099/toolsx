040536f98ce49cdd22ca4ecb6719937c41c13117
2912942099 <2912942099@qq.com>
2026-05-11 16:35:45 +0800

    add sitemap and seo meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://toolsx.vercel.app',
+  site: 'https://toolsx-teal.vercel.app',
+  integrations: [sitemap()],
 });
