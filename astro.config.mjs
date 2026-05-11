b66a4e340aa1242ee2d31500d8fcc0690351f4f1
2912942099 <2912942099@qq.com>
2026-05-11 20:05:49 +0800

    switch to www.kongbyte.top

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://toolsx-teal.vercel.app',
+  site: 'https://www.kongbyte.top/',
   integrations: [sitemap()],
 });
